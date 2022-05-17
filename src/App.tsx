03532dcc1dde5d30ae23ad834847df3ae3c6c44e
EvgeniyCvirko <evgeniy.cvirko.90@mail.ru>
2022-05-17 22:06:38 +0300

    create Counter1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Counter} from './components/Counter'
+import {v1} from "uuid";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+    return (
+        <div>
+            < Counter/>
+        </div>
+    )
 }
-
-export default App;
